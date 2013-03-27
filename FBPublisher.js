8b83f8d85d95149e4e5641f406b67d8848b8f485
Adam Konrád <kixorz@gmail.com>
2013-03-27 01:26:37 -0500

    actions pump facebook batching

--- FBPublisher.js
+++ FBPublisher.js
@@ -1,4 +1,5 @@
 var FBPublisher = {
+	actions: [],
 	permissions: {},
 	
 	init: function init() {
@@ -20,22 +21,44 @@ var FBPublisher = {
 		var self = this;
 		FB.api('/me/permissions/', function getPermissionsResponse(response) {
 			self.permissions = response.data[0];
+			console.log(response, self.permissions);
 		} );
 	},
 	
 	hasPermission: function hasPermission(permission) {
-		return typeof self.permissions[ permission ] !== 'undefined';
+		return typeof this.permissions[ permission ] !== 'undefined';
 	},
 	
-	publish: function publish(action, url) {
-		
+	publish: function publish(action, obj, callback) {
+		this.actions.push( {
+			name: action,
+			obj: obj,
+			callback: typeof callback !== 'function' ? function(){} : callback
+		} );
 	},
 	
 	pump: function pump() {
 		//check if we have publish_actions permission
-		if( hasPermission( 'publish_actions' ) ) {
-			console.log('we have this permission');
+		if(this.hasPermission('publish_actions')) {
+			if(this.actions.length > 0) {
+				var batch = [];
+				var actions = [];
+				for(var i in this.actions) {
+					var action = this.actions[i];
+					batch.push( {
+						method: 'POST',
+						relative_url: 'me/' + action.name,
+						//TODO: add object keys
+						callback: action.callback
+					} );
+				}
+				
+				FB.api('/', 'POST', {
+					batch: batch
+				}, function(response){ console.log(response); });
+			}
 		} else {
+			
 		}
 	}
 };
